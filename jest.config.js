eb9ab3bb7c71a6ef9d1f4ca51716e977c6284228
Ian Menethil <62359909+ianmenethil@users.noreply.github.com>
2025-05-09 11:03:53 +1000

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	testEnvironment: 'jest-environment-jsdom',
-	clearMocks: true,
-	coverageDirectory: 'coverage',
-	transform: { '^.+.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['@babel/preset-env'] }] },
-	transformIgnorePatterns: ['/node_modules/', '\\.pnp\\.[^\\/]+$'],
-	moduleNameMapper: {},
-};
